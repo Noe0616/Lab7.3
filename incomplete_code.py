6e044151fc65d3c6cc7be662484aaa038035e665
Kittathat <45294873+kittathat123@users.noreply.github.com>
2019-03-14 18:38:15 +0700

    Add files via upload

--- incomplete_code.py
+++ incomplete_code.py
@@ -1,4 +1,3 @@
-import abc
 class Transportation(object):
    """Abstract base class"""
 
@@ -8,8 +7,7 @@ class Transportation(object):
       self.start = start
       self.end = end
       self.distance = distance
-      
-   @abc.abstractmethod
+
    def find_cost( self ):
       """Abstract method; derived classes must override"""
       raise NotImplementedError
@@ -21,32 +19,25 @@ class Walk( Transportation ):
       Transportation.__init__( self, start, end, distance)
 
    def find_cost( self ):
-      return 0
+      return self.distance * 80
 
-class Taxi( Transportation ):
+class Boat(Transportation):
    def __init__( self, start, end, distance ):
-      Transportation.__init__( self, start, end, distance)
-   def find_cost( self ):
-      return self.distance * 40
+      Transportation.__init__(self , start , end , distance)
 
-class Train( Transportation ):
-   def __init__( self, start, end, distance , station):
-      Transportation.__init__( self, start, end, distance)
-      self.station = station
-   def find_cost( self ):
-      return self.station * 5
+   def find_cost(self):
+      return self.distance * 5
 
-
-   
 # main program
 
 travel_cost = 0
 
 trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         Train("Ladkrabang Station","Payathai Station",40,6),
-         Taxi("Payathai Station","The British Council",3) ]
+         Boat("KMITL", "KMIDS", 60) ]
+
+
 
 for travel in trip:
    travel_cost += travel.find_cost()
+
 print (travel_cost)
